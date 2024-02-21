536f676eb8a189c07566a8212d6a21cdf1039e3b
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-22 05:52:56 +0700

    Initial commit

---
+++ src/main/java/zuhaproject/redis/StudySpingRedisApplication.java
@@ -0,0 +1,13 @@
+package zuhaproject.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpingRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpingRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zuhaproject/redis/StudySpingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package zuhaproject.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
